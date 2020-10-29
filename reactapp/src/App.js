ab0819a7335cf5e58ad720d3474ad5adf3df9c8a
AdiG <adigollamudi@gmail.com>
2020-10-29 12:56:03 -0400

    react app created

---
+++ reactapp/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className='App'></div>;
+  }
+}
